8c416cbd7d8b55fc49163faf4d887baea7347b1a
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-02-10 00:21:29 +0100

    refined thank you page

--- frontend/src/components/ThankYouPage.js
+++ frontend/src/components/ThankYouPage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import {Container} from 'react-bootstrap'
 
 
 const ThankYouPage = () => {
     return(
         <>
-            <h4>Thank You Page</h4>
+        <Container>
+            <h2 className='mt-5'>Thank you for your order!
+            </h2>
+            <p className='mt-2'>You will receive your package on x/x/x, processing time might take x business days</p>
+            <p>Our customer service will call you for your confirmation</p>
+            <p>If you have questions feel free to contact us</p>
+        </Container>
         </>
     )
 }
