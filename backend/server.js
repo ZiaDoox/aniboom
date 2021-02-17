07b91ca6877d19fa020d207f3e87ba1bc7cff497
Ziad Sabillah <ziadoox.sabillah@gmail.com>
2021-02-17 16:44:43 +0100

    removed paypal integration

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,12 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
+
+// Paypal is not wanted by client
+/*
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
-)
+)*/
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
