0bf2d7dd0a1081aec1efdd0874c4ca29be441ccb
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-03-08 18:52:21 +0100

    added link container when user clicks a hero img

--- frontend/src/components/HeroCarousel.js
+++ frontend/src/components/HeroCarousel.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Carousel, Image } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
@@ -8,9 +8,12 @@ import Message from './Message'
 const HeroCarousel = () => {
   return (
     <Carousel fade={true} pause='hover'className='bg-light carouselP'>
-        <Carousel.Item>
-            <Image src={process.env.PUBLIC_URL + '/images/sliderpets.jpg'} className="heroImg img-fluid" fluid />
-        </Carousel.Item>
+        <LinkContainer to='/products/Chiens'>
+          <Carousel.Item>
+              <Image src={process.env.PUBLIC_URL + '/images/sliderpets.jpg'} className="heroImg img-fluid" fluid />
+          </Carousel.Item>
+        </LinkContainer>
+
         
         <Carousel.Item >
             <Image src={process.env.PUBLIC_URL + '/images/sliderpets2.png'} className="heroImg img-fluid"fluid />
