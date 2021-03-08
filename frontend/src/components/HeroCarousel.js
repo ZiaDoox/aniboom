2f2566df941ad8bb5e02df0038774b0ebf4f21f1
Hamraoui <aelhamraoui748@gmail.com>
2021-03-08 18:36:44 +0100

    added link to category

--- frontend/src/components/HeroCarousel.js
+++ frontend/src/components/HeroCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-bootstrap'
+import { Carousel, Image, Link } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from './Loader'
 import Message from './Message'
@@ -8,9 +8,11 @@ import Message from './Message'
 const HeroCarousel = () => {
   return (
     <Carousel fade={true} pause='hover'className='bg-light carouselP'>
+        <Link to='products/Chiens'>
         <Carousel.Item >
             <Image src={process.env.PUBLIC_URL + '/images/sliderpets.jpg'} className="heroImg img-fluid" fluid />
         </Carousel.Item>
+        </Link>
         
         <Carousel.Item >
             <Image src={process.env.PUBLIC_URL + '/images/sliderpets2.png'} className="heroImg img-fluid"fluid />
