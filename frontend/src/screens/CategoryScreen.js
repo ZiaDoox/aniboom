38f3ac3287319bad7507246091a37dbfb481c85f
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-03-09 16:30:13 +0100

    added sorting feature in category screen

--- frontend/src/screens/CategoryScreen.js
+++ frontend/src/screens/CategoryScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, Container, Button} from 'react-bootstrap'
+import { Row, Col, Container, Button, Form} from 'react-bootstrap'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Meta from '../components/Meta'
@@ -26,21 +26,55 @@ const CategoryScreen = ({ history, match }) => {
     products.filter(function(product) {
       return product.category === category;
     }) : {}
+  
+  const [sortedField, setSortedField] = React.useState(null);
+  let sortedProducts = [...filteredProducts];
+  
+  if(sortedField !== null) {
+    if(sortedField === 'price-d') {
+      sortedProducts.sort((a,b) => {
+        return a.price - b.price;
+      });
+    }else if(sortedField === 'inStock') {
+      sortedProducts = sortedProducts.filter(function(product) {
+        return product.countInStock > 0;
+      })
+    } else if (sortedField === 'price-a') {
+      sortedProducts.sort((a, b) => {
+        return b.price - a.price;
+      });
+    }
+  }
 
   return (
     <>
     <Container>
       <Meta />
-      <h1 className='title'>Espace {category}</h1>
+      <Row>
+        <h1 className='title'>Espace {category}</h1>
+        <Form className='title'>
+          <Form.Label>Trier par:</Form.Label>
+          <Form.Control
+            as='select'
+            onChange={(e) => {
+              setSortedField(e.target.value)
+            }}>
+              <option value=' '></option>
+              <option value='price-d'>Prix - Desc</option>
+              <option value='price-a'>Prix - Asc</option>
+              <option value='inStock'>Disponibilite</option>
+          </Form.Control>
+        </Form>
+      </Row>
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
-      ) : filteredProducts.length ? (
+      ) : sortedProducts.length ? (
         <>
           <Container className="mt-5 mb-5 p-0">
             <Row className="d-flex justify-content-center">
-              {filteredProducts.map((product) => (
+              {sortedProducts.map((product) => (
                 <Col md={10} key={product._id}>
                 <Row className="p-2 border rounded categorysc-card">
                   <Col className="mt-1" md={3}>
