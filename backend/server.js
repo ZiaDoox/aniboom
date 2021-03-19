4e9ff6830ab13ba082b9868e9631fe84a26b204b
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-03-19 18:54:50 +0100

    re-added import to fix bug

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-
+import colors from 'colors';
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -58,6 +58,6 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
   )
 )
